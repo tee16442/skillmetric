5af388edf318958ab81be0c4939b6d9a233171b7
Tee <tee16357@gmail.com>
2023-03-23 16:51:26 +0700

    skillmetric

---
+++ src/main/java/com/ss/skillmetric/SkillmetricApplication.java
@@ -0,0 +1,13 @@
+package com.ss.skillmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillmetricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/skillmetric/SkillmetricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.skillmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmetricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
